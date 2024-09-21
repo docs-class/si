ef9a010ae36aed0544dd9e233d67ef11af045360
Mario R. <mario@iesdoctorbalmis.com>
2024-09-21 23:23:52 +0200

    changing site param in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs-class.si.github.io/',
+	site: 'https://docs-class.github.io/',
 	base: 'si',
 
 	integrations: [
