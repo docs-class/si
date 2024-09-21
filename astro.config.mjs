762ba1b203f1330ad88dc170b3aab7e14aa32ff4
Mario R. <mario@iesdoctorbalmis.com>
2024-09-21 23:17:21 +0200

    SLUG URL fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	site: 'https://docs-class.github.io/si',
 	base: 'si',
-	
+
 	integrations: [
 		starlight({
 			title: 'Sistemas Informáticos',
@@ -19,7 +19,7 @@ export default defineConfig({
 					label: 'UT1 - SISTEMAS INFORMÁTICOS: ARQUITECTURA Y COMPONENTES',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: '1.1 Arquitectura y Componentes', slug: '/ut1/ut11' },
+						{ label: '1.1 Arquitectura y Componentes', slug: 'ut1/ut11' },
 						{ label: '1.2 Tipos de Redes y Componentes', slug: 'ut1/ut12' },
 						{ label: '1.3 Precauciones Básicas', slug: 'ut1/ut13' },
 					],
