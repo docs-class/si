7acd0478e23f047238546dcece0b501d7c772e66
Mario R. <mario@iesdoctorbalmis.com>
2024-09-21 11:42:06 +0200

    adding first example pages to upload to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,10 +13,12 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'UT1 - SISTEMAS INFORMÁTICOS: ARQUITECTURA Y COMPONENTES',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: '1.1 Arquitectura y Componentes', slug: 'ut1/ut11' },
+						{ label: '1.2 Tipos de Redes y Componentes', slug: 'ut1/ut12' },
+						{ label: '1.3 Precauciones Básicas', slug: 'ut1/ut13' },
 					],
 				},
 				{
