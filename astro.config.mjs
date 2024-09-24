dba8ed5a0e51e545a623d057fb20ad9e6c898165
Mario R. <mario@iesdoctorbalmis.com>
2024-09-24 12:42:26 +0200

    Adding markdown components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,32 +3,33 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 
+import markdoc from '@astrojs/markdoc';
+
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs-class.github.io/',
-	base: '/si',
+    site: 'https://docs-class.github.io/',
+    base: '/si',
 
-	integrations: [
-		starlight({
-			title: 'Sistemas Informáticos',
-			social: {
-				github: 'https://github.com/docs-class/si',
-			},
-			sidebar: [
-				{
-					label: 'UT1 - SISTEMAS INFORMÁTICOS: ARQUITECTURA Y COMPONENTES',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: '1.1 Arquitectura y Componentes', slug: 'ut1/ut11' },
-						{ label: '1.2 Tipos de Redes y Componentes', slug: 'ut1/ut12' },
-						{ label: '1.3 Precauciones Básicas', slug: 'ut1/ut13' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [markdoc(),starlight({
+        title: 'Sistemas Informáticos',
+        social: {
+            github: 'https://github.com/docs-class/si',
+        },
+        sidebar: [
+            {
+                label: 'UT1 - SISTEMAS INFORMÁTICOS: ARQUITECTURA Y COMPONENTES',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: '1.1 Arquitectura y Componentes', slug: 'ut1/ut11' },
+                    { label: '1.2 Tipos de Redes y Componentes', slug: 'ut1/ut12' },
+                    { label: '1.3 Precauciones Básicas', slug: 'ut1/ut13' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), markdoc()],
 });
