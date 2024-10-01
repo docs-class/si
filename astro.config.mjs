3937aa71c6189e973836e569cb5f8a8ab7bbef04
Mario R. <mario@iesdoctorbalmis.com>
2024-10-01 12:02:56 +0200

    trying redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,13 +8,15 @@ import markdoc from '@astrojs/markdoc';
 export default defineConfig({
     site: 'https://docs-class.github.io/',
     base: '/si',
-
+    redirects: {
+        'https://docs-class.github.io/si/': 'https://docs-class.github.io/si/va'
+    },
     integrations: [markdoc(), starlight({
         title: {
             es: 'Sistemas Informáticos',
             va: 'Sistemes Informàtics',
             en: 'IT Systems',
-          },
+        },
         social: {
             github: 'https://github.com/docs-class/si',
         },
@@ -28,16 +30,16 @@ export default defineConfig({
         sidebar: [
             {
                 label: 'UT1 - SISTEMES INFORMÀTICS: ARQUITECTURA Y COMPONENTS',
-                translations: { 
-                  'es': 'UT1 - SISTEMAS INFORMÁTICOS: ARQUITECTURA Y COMPONENTES' ,
-                  'en': 'UT1 - IT SYSTEMS: ARQUITECTURE AND COMPONENTS' ,
+                translations: {
+                    'es': 'UT1 - SISTEMAS INFORMÁTICOS: ARQUITECTURA Y COMPONENTES',
+                    'en': 'UT1 - IT SYSTEMS: ARQUITECTURE AND COMPONENTS',
                 },
                 items: [
                     {
                         label: '1.1 Arquitectura i Components',
-                        translations: { 
-                          'es': '1.1 Arquitectura y Componentes' ,
-                          'en': '1.1 Arquitecture and Components' ,
+                        translations: {
+                            'es': '1.1 Arquitectura y Componentes',
+                            'en': '1.1 Arquitecture and Components',
                         },
                         items: [
                             'ut1/ut11',
@@ -45,7 +47,7 @@ export default defineConfig({
                             'ut1/ut11-procesador',
                             'ut1/ut11-placa',
                             'ut1/ut11-caja',
-                            { slug: 'ut1/ut11-ram', badge: { text: 'Current', variant: 'success' }},
+                            'ut1/ut11-ram',
                             'ut1/ut11-gpu',
                             'ut1/ut11-almacenamiento',
                             'ut1/ut11-interfaces',
@@ -55,24 +57,24 @@ export default defineConfig({
                     },
                     {
                         label: '1.2 Tipus de Xarxes i Components',
-                        translations: { 
-                          'es': '1.2 Tipos de Redes y Componentes' ,
-                          'en': '1.2 Network Topologies and Components' ,
+                        translations: {
+                            'es': '1.2 Tipos de Redes y Componentes',
+                            'en': '1.2 Network Topologies and Components',
                         },
                         items: [
                             'ut1/ut12',
                         ],
                     },
                     {
-                      label:'1.3 Precaucions Bàsiques',
-                      translations: { 
-                        'es': '1.3 Precauciones Básicas' ,
-                        'en': '1.3 Basic Precautions' ,
-                      },
-                      items: [
-                          'ut1/ut13',
-                      ],
-                  },
+                        label: '1.3 Precaucions Bàsiques',
+                        translations: {
+                            'es': '1.3 Precauciones Básicas',
+                            'en': '1.3 Basic Precautions',
+                        },
+                        items: [
+                            'ut1/ut13',
+                        ],
+                    },
                 ],
             },
         ],
