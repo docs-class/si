1ab77b5b55aefc01d819766d191b5cb11d82f340
Mario R. <mario@iesdoctorbalmis.com>
2026-03-02 12:47:21 +0100

    Mending ut4 do-while

--- astro.config.mjs
+++ astro.config.mjs
@@ -189,18 +189,19 @@ export default defineConfig({
 
                     
                     //UT5
-                    {
+   /*                 {
                         label: "UT5 - SISTEMAS INFORMÁTICOS EN RED. CONFIGURACIÓN Y EXPLOTACIÓN",
                         // badge: { text: '25-NOV', variant: 'success' },
                         // Collapsa el grupo de forma predeterminada.
                         items: [
                             'ut5/ut51',
-                            'ut5/ut51a-subnetting',
+                            'ut5/ut51a-network-types',
+                            'ut5/ut52a-internet-protocol',
                             // 'ut5/ut51-services',
                             // 'ut5/ut51-ssh',
                         ],
                     },
-   /*                 //UT6
+                    //UT6
                     {
                         label: "UT6 - GESTIÓN DE RECURSOS EN RED",
                         // badge: { text: '25-NOV', variant: 'success' },
